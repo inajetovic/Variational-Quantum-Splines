6c364f6b4d067f5a9293fb9a03f378686b47fa85
filorazi <orazifilippo@gmail.com>
2022-11-23 16:57:43 +0100

    bug fix

--- exper_run.py
+++ exper_run.py
@@ -3,22 +3,17 @@ import pandas as pd
 
 def multiple_experiment(nq, path, max_iter, experiment_number, func):
     res = []
-    pd.DataFrame(columns=[  "Condition number",
-                            "norm(yk)",
-                            "training_cost",
-                            "exe_time",
-                            "in_train_weights",
-                            'RSS_q',
-                            'RSS_h',
-                            'seed'
-                          ]).to_json(path)
+    import os 
     pnq = {'3':(3,8),
           '4':(4,16)}
     for i in range(int(experiment_number)):
-        print(f"{i}th iteration")
+        print(f"Experiment number {i}")
         el = pnq[nq]
-        df = pd.read_json(path)
-        df = pd.concat([df, pd.DataFrame([train_eval(el[0],el[1], func, max_iter )])], ignore_index = True)
+        if not os.path.isfile(path):
+            df = pd.DataFrame([train_eval(el[0],el[1], func, max_iter )])
+        else:
+            df = pd.read_json(path)
+            df = pd.concat([df, pd.DataFrame([train_eval(el[0],el[1], func, max_iter )])], ignore_index = True)
         
         #import os
         #if os.path.isfile(path):
@@ -47,10 +42,6 @@ if __name__=='__main__':
         Running the script without any parameter is the same as running:\n \
         python exper_run.py -sp results.json -mi 300 -en 25 -func sigmoid -nq 3')
         exit()
-    if '-sp' in sys.argv:
-        path = sys.argv[sys.argv.index("-sp")+1]
-    else:
-        path = "results.json"
     if '-mi' in sys.argv:
         mi = sys.argv[sys.argv.index("-mi")+1]
     else:
@@ -67,4 +58,9 @@ if __name__=='__main__':
         nq = sys.argv[sys.argv.index("-nq")+1]
     else:
         nq = '3'
+    if '-sp' in sys.argv:
+        path = sys.argv[sys.argv.index("-sp")+1]
+    else:
+        path = f"results_{func}_{nq}.json"
+
     multiple_experiment(nq, path, mi, en, func)
